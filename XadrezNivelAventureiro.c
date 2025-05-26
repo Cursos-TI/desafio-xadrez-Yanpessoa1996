8cbc101e16d907333b597103b287956cd2bad8ae
Yanpessoa1996 <yanpessoa@hotmail.com>
2025-05-26 15:10:06 +0000

    criação do XadrezNivelAventureiro - Movimento L

---
+++ XadrezNivelAventureiro.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+
+    //Titulo do desafio (Colorido)
+    printf("\033[31mDesafio Nivel Aventureiro - Movimentando o cavalo!\033[0m\n\n");
+
+    //Constante de movimentação do cavalo
+    int const mov_1 = 2; //Primeiro movimento de duas casas
+    int const mov_2 = 1; //Primeiro movimento de uma casa
+
+    //Primeiro movimento do cavalo sempre direcionado a andar 2 casas - Usando For
+    printf("Movimentação cavalo:\n");
+
+    for (int i = 0; i < mov_1; i++)
+    {
+        printf("Baixo\n");
+    }
+    
+    //Segundo movimento do cavalo sempre direcionado a andar 1 casa - Usando While
+    int j = 0;
+
+    while (j < mov_2)
+    {
+        printf("Esquerda\n\n\033[032mMovimento em L completo!\033[0m\n\n");
+        j++;
+    }
+    
+    return 0;
+}
