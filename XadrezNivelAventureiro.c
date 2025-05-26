9c283875f69530692acda96dd09d76a9eaf8ef61
Yanpessoa1996 <yanpessoa@hotmail.com>
2025-05-26 15:49:06 +0000

    COrreção de textos

--- XadrezNivelAventureiro.c
+++ XadrezNivelAventureiro.c
@@ -5,8 +5,8 @@ int main(){
     printf("\033[31mDesafio Nivel Aventureiro - Movimentando o cavalo!\033[0m\n\n");
 
     //Constante de movimentação do cavalo
-    int const mov_1 = 2; //Primeiro movimento de duas casas
-    int const mov_2 = 1; //Primeiro movimento de uma casa
+    int const mov_1 = 2; //Primeiro movimento - Duas casas
+    int const mov_2 = 1; //Primeiro movimento - Uma casa
 
     //Primeiro movimento do cavalo sempre direcionado a andar 2 casas - Usando For
     printf("Movimentação cavalo:\n");
