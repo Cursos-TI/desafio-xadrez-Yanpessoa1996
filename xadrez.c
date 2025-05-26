2f9f0f12d02865ea021a2b630147daa0615fb04b
Yanpessoa1996 <yanpessoa@hotmail.com>
2025-05-26 14:27:41 +0000

    Foi feito incremento de titulo e textos coloridos

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main(){
+    //Titulo do desafio colorido
+    printf("\33[31m");
+
+    printf("Jogo de xadrez, nivel iniciante!\n\n");
+
+    printf("\033[0m");
+
     //Constante para quantidade de movimentos
 int const bispo_mov = 5;
 int const torre_mov = 8;
@@ -9,8 +16,11 @@ int const rainha_mov = 8;
 printf("Movimentação Bispo:\n");
 for (int i = 0; i < bispo_mov; i++)
 {
-    printf("Cima, Direita: Diagonal Superior Direita\n");  
+    //Foi acrescentada a cor verde na movimentação do bispo indicando a conclusão do movimento em diagonal
+    printf("Cima, Direita: \033[32mDiagonal Superior Direita\033[0m\n");  
 }
+
+printf("\n");
 
     //Mover torre cinco casas para direita
 printf("Movimentação Torre:\n");
@@ -19,11 +29,15 @@ for (int i = 0; i < torre_mov; i++)
     printf("Direita\n");
 }
 
+printf("\n");
+
     //Mover rainha cinco casas para esquerda
     printf("Movimentação Rainha:\n");
     for (int i = 0; i < rainha_mov; i++)
     {
         printf("Esquerda\n");
     }
+
+    return 0;
 
 }
